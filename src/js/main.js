49aa10bdee801723564b0ca41bd1aaeddc004ea7
Danila Kiselev <kiselev.danila.03@mail.ru>
2020-03-24 03:33:01 +0300

    fixed tc3

--- src/js/main.js
+++ src/js/main.js
@@ -11,3 +11,17 @@ hamburger.addEventListener('click', (e) => {
   }
 })
 // for hamburger
+const composition = document.querySelector('.off-images');
+const burgers__composition = document.querySelector('.burgers__composition');
+const composition_close = document.querySelector('.composition-close');
+burgers__composition.addEventListener('click', (e) => {
+  if (composition.classList.contains('composition--active')) {
+    composition.classList.remove('composition--active');
+  } else {
+    composition.classList.add('composition--active');
+  }
+})
+composition_close.addEventListener('click', (e) => {
+  composition.classList.remove('composition--active');
+})
+// for composition
